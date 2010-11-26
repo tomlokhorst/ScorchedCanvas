807223a02893645d106981d51f07c2aca2e0b07b
Tom Lokhorst <tom@q42.nl>
2010-11-26 10:46:15 +0100

    Added MVC server.

---
+++ ScorchedServer/ScorchedServer/Global.asax.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ScorchedServer
+{
+  public class MvcApplication : System.Web.HttpApplication
+  {
+    public static void RegisterRoutes(RouteCollection routes)
+    {
+      routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+      routes.MapRoute(
+          "Default",
+          "gameupdates/{session}",
+          new { controller = "Game", action = "Updates", session = UrlParameter.Optional }
+      );
+
+    }
+
+    protected void Application_Start()
+    {
+      AreaRegistration.RegisterAllAreas();
+
+      RegisterRoutes(RouteTable.Routes);
+    }
+  }
+}
