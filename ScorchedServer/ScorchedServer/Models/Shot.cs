a6ef4271efdf2dff5d877b1532e022d6925dc555
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2010-11-26 12:36:41 +0100

    First version of models.

---
+++ ScorchedServer/ScorchedServer/Models/Shot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace ScorchedServer.Models
+{
+    public class Shot
+    {
+        public Weapon Weapon { get; set; }
+        public Point Origin { get; set; }
+        public float Angle { get; set; }
+        public float Power { get; set; }
+    }
+}
---
+++ ScorchedServer/ScorchedServer/Models/World.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Drawing;
+
+namespace ScorchedServer.Models
+{
+    public class World
+    {
+        public Size Size { get; set; }
+        public List<Player> Players { get; set; }
+        public int[] Landscape { get; set; }
+    }
+}
