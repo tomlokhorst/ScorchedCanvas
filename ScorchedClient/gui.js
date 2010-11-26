bced187f0f602c4105711055904dc54c1677944f
Remco Veldkamp <remcoder@blackbook.local>
2010-11-26 12:09:40 +0100

    added gui

---
+++ ScorchedClient/gui.js
@@ -0,0 +1,15 @@
+/*
+
+draws the aim gui.
+handles the mouse events for aiming (mousemove) and firing (mouseup)
+
+*/
+
+var GUI = {
+  
+  // requires screen width/height
+  draw: function()
+  {
+    
+  }
+};
