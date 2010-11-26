5006417ad01b872cf1e74ae42f3760ec22eec65d
Martijn van Steenbergen <martijn@van.steenbergen.nl>
2010-11-26 20:08:38 +0100

    Schietlogica op de server

--- ScorchedServer/ScorchedServer/Models/Shot.cs
+++ ScorchedServer/ScorchedServer/Models/Shot.cs
@@ -12,5 +12,21 @@ namespace ScorchedServer.Models
         public Point Origin { get; set; }
         public float Angle { get; set; }
         public float Power { get; set; }
+
+        public static IEnumerable<Vector> Trace(Vector p, Vector v, Vector a, float mass)
+        {
+            yield return p;
+
+            int dt = 10;
+            
+            while (true)
+            {
+                Vector dv = a.Scale(dt / mass);
+                p = p.Add(v, dt);
+                p = p.Add(dv, dt / 2);
+                v = v.Add(dv);
+                yield return p;
+            }
+        }
     }
 }
