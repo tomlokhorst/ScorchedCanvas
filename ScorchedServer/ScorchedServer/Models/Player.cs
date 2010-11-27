7d899f88f0017915e82f77b0fac011e9e67e997d
Tom Lokhorst <tom@q42.nl>
2010-11-27 16:00:30 +0100

    Die! Player! Die!

--- ScorchedServer/ScorchedServer/Models/Player.cs
+++ ScorchedServer/ScorchedServer/Models/Player.cs
@@ -86,6 +86,9 @@ namespace ScorchedServer.Models
         p.health -= 0.1;
 
         yield return new { type = "updatePlayer", player = p };
+
+        if (p.health <= 0)
+          yield return new { type = "quitPlayer", playerId = p.id };
       }
     }
 
