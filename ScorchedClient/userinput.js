5474ee6c976277a123d8a6841908d53dcff265cb
remcoder <remcoder@gmail.com>
2010-11-27 01:11:10 +0100

    working on iphone/ipad touch events

--- ScorchedClient/userinput.js
+++ ScorchedClient/userinput.js
@@ -19,21 +19,29 @@ var UI = {
     UI.centerx = config.screenSize.width/2;
     UI.centery = config.screenSize.height/2;
     $(canvas).bind("mousemove", UI.aim);
+    $(canvas).bind("touchmove", UI.aim);
     $(canvas).bind("mouseup", UI.fire);
+    $(canvas).bind("touchend", UI.fire);
     $(canvas).bind("mousedown", UI.startAim);
+    $(canvas).bind("touchstart", UI.startAim);
   },
     
-  startAim: function() {
+  startAim: function(evt) {
+    evt.preventDefault();
     world.guiAim = true;
   },
   
   // update aiming
   aim: function(evt) {      
+    evt.preventDefault();
     var offset = $(UI.canvas).offset();
     
+    var xpos = evt.pageX || evt.touches[0].pageX;
+    var ypos = evt.pageY || evt.touches[0].pageY;
+    
     UI.aim = { 
-      x : evt.pageX - offset.left,
-      y : config.screenSize.height - (evt.pageY - offset.top)
+      x : xpos - offset.left,
+      y : config.screenSize.height - (ypos - offset.top)
     };
     
     var x = UI.aim.x;
@@ -48,6 +56,7 @@ var UI = {
   
   // calculate angle / power from current aim
   fire: function(evt) {
+    evt.preventDefault();
     world.guiAim = false;
     console.log("fire");
     UI.socket.send({
