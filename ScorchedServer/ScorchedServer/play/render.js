a1e94f606e9813ad47777bda7b52dde7f7307180
Tom Lokhorst <tom@lokhorst.eu>
2010-11-28 22:20:21 +0100

    Added Rectangle to render, rewrote code to use it.

--- ScorchedServer/ScorchedServer/play/render.js
+++ ScorchedServer/ScorchedServer/play/render.js
@@ -104,15 +104,10 @@ var renderer = {
 			var player = world.players[i];
 
 			var tank = {};
-			tank.top = player.pos + (config.tankHeight / 2);
-			tank.left = player.pos - (config.tankWidth / 2);
-			tank.right = tank.left + config.tankWidth;
-			tank.bottom = player.posy - (config.tankHeight / 2);
-			tank.center = {};
-			tank.center.x = player.pos;
-			tank.center.y = tank.bottom + (config.tankHeight / 2);
+			tank.center = Vector.fromCart(player.pos, player.posy)
+			tank.rect = Rectangle.fromCenter(tank.center, config.tankWidth, config.tankHeight);
 			tank.health = {};
-			tank.health.x = tank.left;
+			tank.health.x = tank.rect.left;
 			tank.barrel = {};
 			tank.health.y = tank.bottom - config.healthIndicatorBottomMargin;
 			tank.health.l = Math.floor(config.tankWidth * player.health);
@@ -133,15 +128,15 @@ var renderer = {
 			// new Tank
 			ctx.fillStyle = player.color;
 			ctx.beginPath();
-			ctx.moveTo(tank.left, tank.bottom);
-			ctx.lineTo(tank.left + config.tankWidth, tank.bottom);
-			ctx.lineTo(tank.left + config.tankWidth - config.tankGapWidth, tank.bottom + config.tankHeight);
-			ctx.lineTo(tank.left + config.tankGapWidth, tank.bottom + config.tankHeight);
+			ctx.moveTo(tank.rect.left, tank.rect.bottom);
+			ctx.lineTo(tank.rect.right, tank.rect.bottom);
+			ctx.lineTo(tank.rect.right - config.tankGapWidth, tank.rect.top);
+			ctx.lineTo(tank.rect.left + config.tankGapWidth, tank.rect.top);
 			ctx.fill();
 
 			
 			ctx.beginPath();
-			ctx.arc(tank.left+(config.tankWidth/2), tank.bottom-(config.tankHeight) + 1 , 2*config.tankWidth/4, Math.PI - (Math.PI / 3.5), 2 * Math.PI + (Math.PI / 3.5), true);
+			ctx.arc(tank.center.x, tank.rect.bottom - tank.rect.height + 1, 2*config.tankWidth/4, Math.PI - (Math.PI / 3.5), 2 * Math.PI + (Math.PI / 3.5), true);
 			ctx.fill();
 
 			// me circle
@@ -375,28 +370,125 @@ var renderer = {
 	}
 };
 
-function Vector(x, y) {
-	this.x = x;
-	this.y = y;
-
-	this.add = function(that, factor) {
-		if (factor === undefined) {
-			factor = 1;
-		}
-		return new Vector(this.x + that.x * factor, this.y + that.y * factor);
-	}
-
-	this.scale = function(factor) {
-		return new Vector(x * factor, y * factor);
-	}
+function Vector(x, y)
+{
+  this.x = x;
+  this.y = y;
+  
+  this.add = function(that, factor)
+  {
+    if (factor === undefined)
+    {
+      factor = 1;
+    }
+    return new Vector(this.x + that.x * factor, this.y + that.y * factor);
+  }
+  
+  this.scale = function(factor)
+  {
+ 	  return new Vector(x * factor, y * factor);
+  }
+  
+  this.rotate = function(angle, origin)
+  {
+    origin = origin || Vector.origin;
+  
+    var dx = this.x - origin.x;
+    var dy = this.y - origin.y;
+  
+    var dist = Math.sqrt(dx * dx + dy * dy);
+    var a1 = Math.atan2(dy, dx);
+    var a2 = a1 + angle;
+    var dx2 = Math.cos(a2) * dist;
+    var dy2 = Math.sin(a2) * dist;
+  
+    return new Vector(origin.x + dx2, origin.y + dy2);
+  }
+  
+  this.toString = function ()
+  {
+    return "Vector { x: " + this.x + ", y: " + this.y + "}";
+  }
 }
 
-Vector.fromPolar = function(th, r) {
-	return Vector.fromCart(Math.cos(th) * r, Math.sin(th) * r);
-};
 
-Vector.fromCart = function(x, y) {
+
+Vector.fromPolar = function(th, r)
+{
+	return Vector.fromCart(Math.cos(th) * r, Math.sin(th) * r);
+}
+
+Vector.fromCart = function(x, y)
+{
 	return new Vector(x, y);
-};
+}
 
 Vector.origin = Vector.fromCart(0, 0);
+
+// Immutable
+function Rectangle(top, left, right, bottom)
+{
+  this.top         = top;
+  this.left        = left;
+  this.right       = right;
+  this.bottom      = bottom;
+  this.leftTop     = Vector.fromCart(left, top);
+  this.rightTop    = Vector.fromCart(right, top);
+  this.leftBottom  = Vector.fromCart(left, bottom);
+  this.rightBottom = Vector.fromCart(right, bottom);
+
+  this.width  = this.right - this.left;
+  this.height = this.top - this.bottom;
+  this.center = Vector.fromCart(this.left + this.width / 2, this.bottom + this.height /2);
+
+  this.rotate = function(angle, origin)
+  {
+    origin =
+        origin && origin.x !== undefined && origin.y !== undefined ? origin
+      : origin == "top"  ? Vector.fromCart(this.center.x, this.top)
+      : origin == "left" ? Vector.fromCart(this.left, this.center.y)
+      : origin == "right" ? Vector.fromCart(this.right, this.center.y)
+      : origin == "bottom" ? Vector.fromCart(this.center.x, this.bottom)
+      : origin == "leftTop" ? this.leftTop
+      : origin == "rightTop" ? this.rightTop
+      : origin == "leftBottom" ? this.leftBottom
+      : origin == "rightBottom" ? this.rightBottom
+      : this.center;
+
+    var v1 = this.leftTop.rotate(angle, origin);
+    var v2 = this.rightBottom.rotate(angle, origin);
+
+    return Rectangle.fromVectors(v1, v2);
+  }
+
+  this.toString = function ()
+  {
+    return "Rectangle { top: " + this.top + ", left: " + this.left +  ", right: " + this.right +  ", bottom: " + this.bottom + "}";
+  }
+}
+
+Rectangle.fromPoints = function (top, left, right, bottom)
+{
+  return new Rectangle(top, left, right, bottom);
+}
+
+Rectangle.fromCenter = function (center, width, height)
+{
+  var top    = center.y + height / 2;
+  var left   = center.x - width / 2;
+  var right  = center.x + width / 2;
+  var bottom = center.y - height / 2;
+
+  return new Rectangle(top, left, right, bottom);
+}
+
+Rectangle.fromVectors = function (v1, v2)
+{
+  var top    = v1.y > v2.y ? v1.y : v2.y;
+  var left   = v1.x < v2.x ? v1.x : v2.x;
+  var right  = v1.x > v2.x ? v1.x : v2.x;
+  var bottom = v1.y < v2.y ? v1.y : v2.y;
+
+  return new Rectangle(top, left, right, bottom);
+}
+
