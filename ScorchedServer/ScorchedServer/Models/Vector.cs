11ac1d73bb2e0c65274e74bebfce5a6e1c38ae29
Tom Lokhorst <tom@lokhorst.eu>
2010-11-29 20:25:10 +0100

    Added operator overloading for Vector.

--- ScorchedServer/ScorchedServer/Models/Vector.cs
+++ ScorchedServer/ScorchedServer/Models/Vector.cs
@@ -5,46 +5,98 @@ using System.Web;
 
 namespace ScorchedServer.Models
 {
-    public class Vector
+  public class Vector
+  {
+    private readonly double x;
+    private readonly double y;
+
+    public double X { get { return x; } }
+    public double Y { get { return y; } }
+
+    private Vector(double x, double y)
     {
-        private readonly double _x;
-        private readonly double _y;
-
-        public double X { get { return _x; } }
-        public double Y { get { return _y; } }
-
-        public double Theta { get { return Math.Atan2(X, Y); } }
-        public double Radius { get { return Math.Sqrt(X * X + Y * Y); } }
-
-        private Vector(double x, double y)
-        {
-            _x = x;
-            _y = y;
-        }
-
-        public static Vector FromCart(double x, double y)
-        {
-            return new Vector(x, y);
-        }
-
-        public static Vector FromPolar(double th, double r)
-        {
-            return new Vector(Math.Cos(th) * r, Math.Sin(th) * r);
-        }
-
-        public Vector Add(Vector that)
-        {
-            return Add(that, 1);
-        }
-
-        public Vector Add(Vector that, double factor)
-        {
-            return FromCart(this.X + that.X * factor, this.Y + that.Y * factor);
-        }
-
-        public Vector Scale(double factor)
-        {
-            return FromCart(this.X * factor, this.Y * factor);
-        }
+      this.x = x;
+      this.y = y;
     }
+
+    public double Theta()
+    {
+      return Math.Atan2(X, Y);
+    }
+
+    public double Radius()
+    {
+      return Math.Sqrt(X * X + Y * Y);
+    }
+
+    public Vector Add(Vector that)
+    {
+      return FromCart(this.X + that.X, this.Y + that.Y);
+    }
+
+    public Vector Add(double dx, double dy)
+    {
+      return FromCart(this.X + dx, this.Y + dy);
+    }
+
+    public Vector Rotate(double angle)
+    {
+      return Rotate(angle, Vector.Origin());
+    }
+
+    public Vector Rotate(double angle, Vector origin)
+    {
+      var delta = this - origin;
+
+      var dx = Math.Cos(angle) * delta.X - Math.Sin(angle) * delta.Y;
+      var dy = Math.Cos(angle) * delta.X + Math.Sin(angle) * delta.Y;
+
+      return new Vector(origin.X + dx, origin.Y + dy);
+    }
+
+    public Vector Add(Vector that, double factor)
+    {
+      return FromCart(this.X + that.X * factor, this.Y + that.Y * factor);
+    }
+
+    public Vector Scale(double factor)
+    {
+      return FromCart(this.X * factor, this.Y * factor);
+    }
+
+    public override String ToString()
+    {
+      return string.Format("{{ X: {0}, Y: {1} }}", this.X, this.Y);
+    }
+
+    public static Vector FromCart(double x, double y)
+    {
+      return new Vector(x, y);
+    }
+
+    public static Vector FromPolar(double th, double r)
+    {
+      return new Vector(Math.Cos(th) * r, Math.Sin(th) * r);
+    }
+
+    public static Vector operator +(Vector v1, Vector v2)
+    {
+      return Vector.FromCart(v1.X + v2.X, v1.Y + v2.Y);
+    }
+
+    public static Vector operator -(Vector v1, Vector v2)
+    {
+      return Vector.FromCart(v1.X - v2.X, v1.Y - v2.Y);
+    }
+
+    public static Vector operator *(Vector v1, double factor)
+    {
+      return Vector.FromCart(v1.X * factor, v1.Y * factor);
+    }
+
+    public static Vector Origin()
+    {
+      return Vector.FromCart(0, 0);
+    }
+  }
 }
